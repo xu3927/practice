87e4a4f2c49001bdd68f6839752829111e19d1c8
xuzhichao <xuchao2015@foxmail.com>
2016-12-29 18:18:19 +0800

    update

---
+++ angular/app/index.js
@@ -0,0 +1,2 @@
+require('./lib/angular-1.5.8/angular.js');
+require('./lib/angular-ui-router.js');
---
+++ angular/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+// 定义文件夹路径, 转为绝对路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var DIST_PATH = path.resolve(ROOT_PATH, './dist');
+console.log('__dirname', __dirname);
+module.exports = {
+	entry: APP_PATH,
+	output: {
+		path: DIST_PATH,
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new HtmlwebpackPlugin({
+			title: 'angular项目结合webpack练习',
+			template: './app/index.ejs'
+		})
+	],
+	devServer: {
+		historyApiFallback: true,
+	 	hot: true,
+	 	inline: true,
+	 	progress: true,
+	 	port: 9000
+	 },
+	  devtool: 'eval-source-map',
+	  module: {
+	  	loaders: [
+	  	
+	  	]
+	  }
+}
