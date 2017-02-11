79f83fffc864695b94915ce9ee8468d48e259025
xuzhichao <xuchao2015@foxmail.com>
2017-02-11 22:45:22 +0800

    update

--- fileServer/index.js
+++ fileServer/index.js
@@ -1,13 +1,12 @@
 /**
  * Created by 3927 on 2017/2/7 1:06.
  */
-
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var port = 3000;
 var File = require('file');
-var filePath = '../files';
-
+var filePath = '../h5';
 
 var mp4Files = readAllMp4Files();
 serve(mp4Files);
@@ -15,12 +14,14 @@ serve(mp4Files);
 function readAllMp4Files () {
     var mp4FilesArr = [];
     File.walkSync(filePath, function (dirPath, dirs, files) {
+        console.log('dirPath:', dirPath);
         if (files.length > 0) {
             for (var i = 0, l = files.length; i < l; i++) {
                 if (files[i].indexOf('.mp4') === files[i].length - 4) {
-                    var _l = filePath.length + 1;
-                    var _dirPath = dirPath.slice(9);
-                    mp4FilesArr.push(_dirPath + '\\' + files[i]);
+                    var _l = filePath.length;
+                    var _dirPath = dirPath.slice(_l);
+                    console.log('_dirPath:', _dirPath);
+                    mp4FilesArr.push('video' + _dirPath + '/' + files[i]);
                 }
             }
         }
@@ -29,12 +30,28 @@ function readAllMp4Files () {
 }
 
 function serve (mp4Files) {
-    app.use(express.static(filePath));
-    app.use(express.static('static'));
     // 设置视图模板
     app.set('view engine', 'ejs');
     // 设置视图文件夹
     app.set("views","./views");
+
+    // 中间件
+    app.use(function (req, res, next) {
+        console.log('访问:', req.url);
+        next();
+    });
+    // 请求video文件(MP4)
+    app.get('/video/*', function (req, res){
+        var fileDir = req.url.slice(7);
+        var stream = fs.createReadStream(filePath + '/' + fileDir);
+        console.log('stream:', stream);
+        stream.pipe(res);
+    });
+    app.use(express.static(filePath));
+    app.use(express.static('static'));
+    /**
+     * 路由列表
+     */
     // 请求所有的mp4文件列表
     app.post('/mp4list', function(req, res) {
        res.send({
@@ -42,11 +59,10 @@ function serve (mp4Files) {
            data: mp4Files
        })
     });
-    // app.get('/', function (req, res){
-    //     // res.render('index.ejs', {
-    //     //     videos: mp4Files
-    //     // });
-    // });
+    // 默认页面
+    app.all('*', function (req, res) {
+       res.send('没有这个页面');
+    });
     app.listen(port);
     console.log('服务器已建立, 监听端口' + port);
 }
